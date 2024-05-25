4b720b3f059afbb05d584d17d01c0df12782100b
Jungin Yoo <frog7134@gmail.com>
2024-05-25 21:16:32 +0900

    main 없는 html 페이지에서 오류 발생 해결

--- client/layout.js
+++ client/layout.js
@@ -2,9 +2,14 @@
 
 // 창 크기 조정 함수
 function adjustLayout() {
-  const headerHeight = document.querySelector(".header").offsetHeight;
-  const mainHeight = window.innerHeight - headerHeight;
-  document.querySelector(".main").style.height = `${mainHeight}px`;
+    const header = document.querySelector(".header");
+    const main = document.querySelector(".main");
+
+    if (header && main) {
+        const headerHeight = header.offsetHeight;
+        const mainHeight = window.innerHeight - headerHeight;
+        main.style.height = `${mainHeight}px`;
+    }
 }
 
 // 창 크기 조정 이벤트 핸들러
@@ -13,15 +18,15 @@ window.addEventListener("load", adjustLayout);
 
 // 창 최소 크기 설정
 window.addEventListener("resize", function () {
-  if (window.innerWidth < 800) {
-    document.body.style.width = "800px";
-  } else {
-    document.body.style.width = "100%";
-  }
+    if (window.innerWidth < 800) {
+        document.body.style.width = "800px";
+    } else {
+        document.body.style.width = "100%";
+    }
 
-  if (window.innerHeight < 600) {
-    document.body.style.height = "600px";
-  } else {
-    document.body.style.height = "100%";
-  }
+    if (window.innerHeight < 600) {
+        document.body.style.height = "600px";
+    } else {
+        document.body.style.height = "100%";
+    }
 });
