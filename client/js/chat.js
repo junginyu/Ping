64226aa3ded91316893cf9a712ed3c3c6570d69b
Jungin Yoo <frog7134@gmail.com>
2024-05-29 00:23:42 +0900

    캐릭터 초기 선택 시 기본 메시지 추가

--- client/js/chat.js
+++ client/js/chat.js
@@ -55,31 +55,30 @@ function displayMessage(message, type, character) {
         chatHistory[character] = [];
     }
 
-    // 채팅 기록에 메시지 추가
-    chatHistory[character].push({ type: type, text: message });
+    chatHistory[character].push({ type: type, text: message }); // 채팅 기록에 메시지 추가
     localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
 
-    // 채팅 컨테이너 엘리먼트를 선택
-    const chatContainer = document.getElementById("chat-history");
+    const chatContainer = document.getElementById("chat-history"); // 채팅 컨테이너 엘리먼트를 선택
 
-    // 새 메시지 엘리먼트 생성
-    const messageElement = document.createElement("div");
+    const messageElement = document.createElement("div"); // 새 메시지 엘리먼트 생성
     messageElement.classList.add("message", type === "user" ? "user" : "assistant");
 
-    // 텍스트 컨테이너 엘리먼트 생성 및 설정
-    const textContainer = document.createElement("div");
+    const textContainer = document.createElement("div"); // 텍스트 컨테이너 엘리먼트 생성 및 설정
     textContainer.classList.add("text-container");
     textContainer.textContent = message;
 
-    // 메시지 엘리먼트에 텍스트 컨테이너를 추가
-    messageElement.appendChild(textContainer);
+    messageElement.appendChild(textContainer); // 메시지 엘리먼트에 텍스트 컨테이너를 추가
 
-    // 메시지 엘리먼트를 채팅 컨테이너에 추가
-    chatContainer.prepend(messageElement);
+    chatContainer.prepend(messageElement); // 메시지 엘리먼트를 채팅 컨테이너에 추가
     chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    const currentCharacter = localStorage.getItem("currentCharacter");
+    if (currentCharacter) {
+        sendInitialMessage(currentCharacter); // 초기 메시지 전송 및 채팅 기록 로드
+    }
+
     const chatInput = document.getElementById("chat-input");
     const sendButton = document.getElementById("send-button");
 
@@ -101,6 +100,23 @@ document.addEventListener("DOMContentLoaded", function () {
         loadChatHistory(currentCharacter); // 로드 시 이전 대화 내역을 불러옵니다.
     }
 });
+
+function sendInitialMessage(character) {
+    const initialMessagesText = {
+        haily: "Hi! I'm Haily. What do you want to talk about?",
+        jun: "私はジュンといいます。私とどんな話をしたいですか？",
+        linda: "¡Hola, amigo! Soy Linda. ¿De qué podemos hablar?",
+    };
+    let chatHistory = JSON.parse(localStorage.getItem("chatHistory") || "{}");
+
+    if (!chatHistory[character] || chatHistory[character].length === 0) {
+        chatHistory[character] = chatHistory[character] || [];
+        chatHistory[character].push({ type: "assistant", text: initialMessagesText[character] });
+        localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
+
+        loadChatHistory(character);
+    }
+}
 
 function loadChatHistory(character) {
     const chatHistory = JSON.parse(localStorage.getItem("chatHistory") || "{}")[character] || [];
